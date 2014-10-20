e2a6ee83e5c0728c89661769abfd251c7a0fe9ad
Ryan Endacott <rzeg24@gmail.com>
2014-10-19 17:34:59 -0700

    Gets credentials now.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,7 +2,10 @@ $(document).ready(grab_credentials);
 $(document).on('page:load', grab_credentials);
 
 function grab_credentials() {
-  alert(get_page());
+  if (get_page() == 'credentials') {
+    var uid = $('#uid').val();
+    var api_token = $('#apitoken').val();
+  }
 }
 
 function get_page() {
