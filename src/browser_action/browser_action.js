632f8166eed82c37e70aecf380381c092347141c
Ryan Endacott <rzeg24@gmail.com>
2014-10-30 21:24:38 -0700

    Show login form if the user isn't logged in.

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,6 +1,32 @@
-$(document).ready(hook_send_payment_button_up)
+$(document).ready(function() {
+  chrome.storage.sync.get('uid', function(data) {
+    if (data.uid) {
+      hook_send_payment_button_up();
+    } else {
+      show_login_form();
+    }
+  });
+
+  hook_up_links();
+});
+
+function hook_up_links() {
+  $('a').click(function(event) {
+    var clicked_element = $(event.target);
+    var url = clicked_element.parent('a')[0].href;
+    chrome.tabs.create({url: url});
+  })
+}
+
+function show_login_form() {
+  $('body').height(100);
+  $('#login_form').show();
+}
 
 function hook_send_payment_button_up() {
+
+  $('body').height(300);
+  $('#payment_form').show();
 
   var output_alert = $('#alert_message');
 
