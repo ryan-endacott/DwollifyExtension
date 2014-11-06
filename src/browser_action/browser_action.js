2cc755c5faf64c3133bbfa9d290a651e009a92cc
Ryan Endacott <rzeg24@gmail.com>
2014-11-05 23:30:09 -0800

    Basic input validation.

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -33,8 +33,6 @@ function hook_send_payment_button_up() {
   $('body').height(300);
   $('#payment_form_container').show();
 
-  var output_alert = $('#alert_message');
-
   $('#payment_form').submit(function() {
     console.log('here');
 
@@ -42,23 +40,40 @@ function hook_send_payment_button_up() {
     $('#payment_form_container').append(spinner.el);
     $('#dimmer').show();
 
+    // Validate inputs
+    var email = $('#email').val();
+    if (!/^[^@]+@[^@]+\.[^@]+$/.test(email)) {
+      show_alert_message('Please enter a valid email.', false);
+      spinner.stop();
+      $('#dimmer').hide();
+      return false;
+    }
+
+    var amount = $('#amount').val();
+    if (!/^\d*\.?\d+$/.test(amount)) {
+      show_alert_message('Please enter a valid amount.', false);
+      spinner.stop();
+      $('#dimmer').hide();
+      return false;
+    }
+
+    var pin = $('#pin').val();
+    if (pin.length != 4) {
+      show_alert_message('Please enter a valid pin.', false);
+      spinner.stop();
+      $('#dimmer').hide();
+      return false;
+    }
+
     chrome.runtime.sendMessage({
       route: 'send_payment',
-      email: $('#email').val(),
-      amount: $('#amount').val(),
-      pin: $('#pin').val()
+      email: email,
+      amount: amount,
+      pin: pin
     }, function(response) {
 
-      output_alert.removeClass('alert-danger');
-      output_alert.removeClass('alert-success');
+      show_alert_message(response.message, response.success);
 
-      if (response.success) {
-        output_alert.addClass('alert-success');
-      } else {
-        output_alert.addClass('alert-danger');
-      }
-      output_alert.text(response.message);
-      output_alert.show();
       spinner.stop();
       $('#dimmer').hide();
 
@@ -67,6 +82,22 @@ function hook_send_payment_button_up() {
     // Don't actually submit form.
     return false;
   });
+}
+
+// Shows message in green if success is true, red if false.
+function show_alert_message(message, success) {
+  var output_alert = $('#alert_message');
+
+  output_alert.removeClass('alert-danger');
+  output_alert.removeClass('alert-success');
+
+  if (success) {
+    output_alert.addClass('alert-success');
+  } else {
+    output_alert.addClass('alert-danger');
+  }
+  output_alert.text(message);
+  output_alert.show();
 }
 
 
